58bd3aaa53fe77274fcd72b8d20f2c6aef728124
t4ylo <thpearce@ucsc.edu>
2026-06-09 19:13:58 -0600

    added button noise

--- Assets/Scripts/ButtonSwitch.cs
+++ Assets/Scripts/ButtonSwitch.cs
@@ -6,22 +6,40 @@ public class ButtonSwitch : MonoBehaviour
     public Material redMaterial;
     public Material greenMaterial;
 
+    public AudioClip buttonSound;
+
     public bool isActivated = false;
 
     private Renderer buttonRenderer;
+    private AudioSource audioSource;
 
     void Start()
     {
         buttonRenderer = GetComponent<Renderer>();
+        audioSource = GetComponent<AudioSource>();
+
         buttonRenderer.material = redMaterial;
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") && Keyboard.current.eKey.wasPressedThisFrame)
+        if (other.CompareTag("Player") &&
+            Keyboard.current.eKey.wasPressedThisFrame &&
+            !isActivated)
         {
-            isActivated = true;
-            buttonRenderer.material = greenMaterial;
+            ActivateButton();
+        }
+    }
+
+    void ActivateButton()
+    {
+        isActivated = true;
+
+        buttonRenderer.material = greenMaterial;
+
+        if (buttonSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(buttonSound);
         }
     }
 }
